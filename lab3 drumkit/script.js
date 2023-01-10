901a6c028ab3111e3581625228ce046bb73aec07
kKrusinski <79981830+kKrusinski@users.noreply.github.com>
2023-01-11 00:41:45 +0100

    drumkit nagrywanie i odtwarzanie

--- lab3 drumkit/script.js
+++ lab3 drumkit/script.js
@@ -1,54 +1,63 @@
 const btnStartRecording = document.getElementById("btn-start-recording");
-//let timer = 
+let recording = false;
+let trackNumber = 1;
 
 document.addEventListener('keypress', (event) => {
     playSound(event.key);
-    startRecording(event.key);
 });
 
 function playSound(key) {
     switch (key) {
         case '1':
             var boom = new Audio('sounds/boom.wav');
+            boom.currentTime = 0;
             boom.play();
             break;
 
         case '2':
             var clap = new Audio('sounds/clap.wav');
+            clap.currentTime = 0;
             clap.play();
             break;
 
         case '3':
             var hihat = new Audio('sounds/hihat.wav');
+            hihat.currentTime = 0;
             hihat.play();
             break;
 
         case '4':
             var kick = new Audio('sounds/kick.wav');
+            kick.currentTime = 0;
             kick.play();
             break;
 
         case '5':
             var openhat = new Audio('sounds/openhat.wav');
+            openhat.currentTime = 0;
             openhat.play();
             break;
 
         case '6':
             var ride = new Audio('sounds/ride.wav');
+            ride.currentTime = 0;
             ride.play();
             break;
 
         case '7':
             var snare = new Audio('sounds/snare.wav');
+            snare.currentTime = 0;
             snare.play();
             break;
         case '8':
             var tink = new Audio('sounds/tink.wav');
+            tink.currentTime = 0;
             tink.play();
             break;
 
         case '9':
             var tom = new Audio('sounds/tom.wav');
+            tom.currentTime = 0;
             tom.play();
             break;
 
@@ -58,36 +67,53 @@ function playSound(key) {
 }
 
 
-let mediaRecorder;
-let audioContext;
-let audioElement;
-let sourceNode;
-let streamDestination;
-let recordedBlobs = [];
+let track1 = [];
+let track2 = [];
+let track3 = [];
+let track4 = [];
 
-audioElement = document.getElementById("audioElement");
+function record(event) {
+    if (!recording) return;
+    const sound = {
+        key: event.key,
+        time: Date.now()
+    };
+   const trackName = eval("track"+ trackNumber);
+    trackName.push(sound);
+    console.log(track1)
+}
+function setRecording(number) {
+    recording = true;
+    if (number) {
+        trackNumber = number;
+    }
+}
+window.addEventListener('keypress', record);
 
-audioElement.onloadedmetadata = function(e) {
-  audioContext = new AudioContext();
-  sourceNode = audioContext.createMediaElementSource(audioElement);
-  streamDestination = audioContext.createMediaStreamDestination();
-  sourceNode.connect(streamDestination);
-  mediaRecorder = new MediaRecorder(streamDestination.stream);
-  mediaRecorder.ondataavailable = handleDataAvailable;
+
+function playBack(number){
+let pauseTime;
+const trackName = eval("track"+ number);
+trackName.forEach(element => {
+    if (element === trackName[0]) {
+        pauseTime = element.time;   
+    }
+    if (element.key) {
+        setTimeout(() => {
+            playSound(element.key);
+        }, element.time - pauseTime);
+    }
+console.log(pauseTime)
+});
+
+
 }
 
-function startRecording() {
-  recordedBlobs = [];
-  mediaRecorder.start();
+function playAll(){
+playBack(1);
+playBack(2);
+playBack(3);
+playBack(4);
 }
 
-function stopRecording() {
-  mediaRecorder.stop();
-}
-
-function handleDataAvailable(event) {
-  if (event.data.size > 0) {
-    recordedBlobs.push(event.data);
-  }
-}
 
