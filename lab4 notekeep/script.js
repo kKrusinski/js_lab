cc9abf1877dfa2ccf74350fcac32cb18fab06122
kKrusinski <79981830+kKrusinski@users.noreply.github.com>
2023-01-16 22:08:54 +0100

    notekeep bez pina + slider

--- lab4 notekeep/script.js
+++ lab4 notekeep/script.js
@@ -1,60 +1,122 @@
-let notes = [];
-let currentEditingNote = null;
+let noteInput
+let errorInfo
+let addBtn
+let ulList
+let newNote
 
-function createNote(title, content, color) {
-    const note = {
-        title: title,
-        content: content,
-        color: color,
-        pin: false,
-        createdAt: new Date()
-    };
-    localStorage.setItem(note.createdAt.toString(), JSON.stringify(note));
-    return note;
+
+let popup
+let popupInfo
+let noteToEdit
+let popupInput
+let popupAddBtn
+let popupClosebtn
+
+
+
+const main = () => {
+    prepareDOMElements()
+    prepareDOMEvents()
 }
 
-function getNotes() {
-    notes = [];
-    for (let i = 0; i < localStorage.length; i++) {
-        const key = localStorage.key(i);
-        if (key !== "loglevel:webpack-dev-server") {
-            const note = JSON.parse(localStorage.getItem(key));
-            notes.push(note);
-        }
+const prepareDOMElements = () => {
+    noteInput = document.querySelector('.note-input')
+    errorInfo = document.querySelector('.error-info')
+    addBtn = document.querySelector('.btn-add')
+    ulList = document.querySelector('.notelist ul')
+
+    popup = document.querySelector('.popup')
+    popupInfo = document.querySelector('.popup-info')
+    popupInput = document.querySelector('.popup-input')
+    popupAddBtn = document.querySelector('.accept')
+    popupClosebtn = document.querySelector('.cancel')
+}
+
+const prepareDOMEvents = () => {
+    addBtn.addEventListener('click', addNewNote)
+    ulList.addEventListener('click', checkClick)
+    popupClosebtn.addEventListener('click', closePopup)
+    popupAddBtn.addEventListener('click', changeNoteText)
+    
+
+
+}
+
+const addNewNote = () => {
+    if (noteInput.value !== '') {
+        newNote = document.createElement('li');
+        newNote.textContent = noteInput.value
+        createToolsArea()
+        ulList.append(newNote)
+       
+       
+       
+       
+        noteInput.value = ''
+        errorInfo.textContent =''
+    } else {
+        errorInfo.textContent = 'Wpisz treść zadania!'
     }
-    return notes.sort((a, b) => b.pin - a.pin)
 }
 
-function renderNotes() {
-  notes = getNotes();
-  const noteList = document.getElementById("note-list");
-  noteList.innerHTML = "";
-  for (const note of notes) {
-      const noteElem = document.createElement("div");
-      noteElem.classList.add("note");
-      noteElem.style.backgroundColor = note.color;
-      if (!currentEditingNote && note.pin) {
-          noteElem.style.border = "solid 2px #000";
-          noteElem.style.zIndex = 1;
-      }
-      noteElem.innerHTML = `<h2>${note.title}</h2><p>${note.content}</p>`;
-      const editButton = document.createElement("button");
-      editButton.innerHTML = "Edit";
-      editButton.addEventListener("click", function() {
-          currentEditingNote = note;
-          document.getElementById("edit-note-title").value = note.title;
-          document.getElementById("edit-note-content").value = note.content;
-          document.getElementById("edit-note-container").style.display = "block";
-      });
-      noteElem.appendChild(editButton);
-      const pinButton = document.createElement("button");
-      pinButton.innerHTML = "Pin/Unpin";
-      pinButton.addEventListener("click", function() {
-          note.pin = !note.pin;
-          localStorage.setItem(note.createdAt.toString(), JSON.stringify(note));
-          renderNotes();
-      });
-      noteElem.appendChild(pinButton);
-      noteList.appendChild(noteElem);
-  }
+const createToolsArea = () => {
+    const toolsPanel = document.createElement('div')
+    toolsPanel.classList.add('tools')
+    newNote.append(toolsPanel)
+
+    const completeBtn = document.createElement('button')
+    completeBtn.classList.add('complete')
+    completeBtn.innerHTML = '<i class="fas fa-check"></i>'
+
+    const editBtn = document.createElement('button')
+    editBtn.classList.add('edit')
+    editBtn.textContent = 'EDIT'
+
+    const deleteBtn = document.createElement('button')
+    deleteBtn.classList.add('delete')
+    deleteBtn.innerHTML = '<i class="fas fa-times"></i>'
+
+    toolsPanel.append(completeBtn,editBtn,deleteBtn)
 }
+
+const checkClick = e => {
+   if(e.target.matches('.complete')){
+  e.target.closest('li').classList.toggle('completed')
+  e.target.classList.toggle('completed')
+   }else if(e.target.matches('.edit')) {
+    editNote(e)
+   }else if (e.target.matches('.delete')){
+    deleteNote(e)
+   }
+   }
+
+const editNote = e => {
+   noteToEdit = e.target.closest('li')
+
+   popupInput.value = noteToEdit.firstChild.textContent
+
+   console.log(noteToEdit.firstChild);
+    popup.style.display = 'flex'
+}
+
+const closePopup = () => {
+    popup.style.display = 'none'
+    popupInfo.textContent = ''
+}
+
+
+const changeNoteText = () => {
+    if(popupInput.value !== ''){
+        noteToEdit.firstChild.textContent = popupInput.value
+        popup.style.display = 'none'
+        popupInfo.textContent = ''
+    } else {
+        popupInfo.textContent = 'Musisz podać jakąś treść!'
+    }
+}
+
+
+const deleteNote = (e) => {
+    e.target.closest('li').remove()
+}
+document.addEventListener('DOMContentLoaded', main)
